301eac62d8697b80c9507c5bd199ee1e53b5b27d
Mashanz <mangatkk@gmail.com>
2024-08-27 20:33:39 +0700

    add lock method

--- src/app_blog/urls.py
+++ src/app_blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
+# make sure di lock setiap url atleast maksimal 1-2 method saja.
 urlpatterns = [
-    path("", views.posts_read_all),
-    path("<str:posts_id>", views.posts_read_one),
-    path("create", views.posts_create),
-    path("<str:posts_id>/update", views.posts_update),
-    path("<str:posts_id>/delete", views.posts_delete)
+    path("", views.posts_read_all),                        # GET all data           -> /
+    path("id/<str:posts_id>", views.posts_read_one),       # GET one data           -> /id/(123)
+    path("create", views.posts_create),                    # POST one data          -> /(create)
+    path("id/<str:posts_id>/update", views.posts_update),  # PATCH one data         -> /id/(123)/update
+    path("id/<str:posts_id>/delete", views.posts_delete)   # DELETE one data        -> /id/(123)/delete
 ]
--- src/app_blog/views.py
+++ src/app_blog/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.template.context_processors import csrf
 from django.db import connection
+from django.views.decorators.http import require_http_methods
 
 # INSERT DATA
+@require_http_methods(["POST"])
 def posts_create(request):
     title = "Judul Lain"
     content = "Kontent Lain"
@@ -19,6 +21,7 @@ def posts_create(request):
         "description": "CREATE"
     })
 
+@require_http_methods(["GET"])
 def posts_read_all(request):
     csrf_generate = csrf(request)
     csrf_value = csrf_generate['csrf_token']  
@@ -36,22 +39,32 @@ def posts_read_all(request):
         "token": f"{csrf_value}"
     })
 
+@require_http_methods(["GET"])
 def posts_read_one(request, posts_id):
+    # tambahkan validasi posts_id adalah uuid bukan integer atau str
+    # ...
 
     with connection.cursor() as c:
         c.execute("SELECT * FROM posts WHERE id = %s", [posts_id])
         row = c.fetchone()
 
     print(row)
+    title = ""
+    content = ""
+    
+    if row:
+        title = row[1]
+        content = row[2]
 
     return JsonResponse({
         "method": "GET",
         "description": "READ ONE",
         "data": posts_id,
-        "title": row[1],
-        "content": row[2]
+        "title": title,
+        "content": content
     })
 
+@require_http_methods(["PATCH"])
 def posts_update(request, posts_id):
     return JsonResponse({
         "method": "PATCH",
@@ -59,6 +72,7 @@ def posts_update(request, posts_id):
         "data": posts_id
     })
 
+@require_http_methods(["DELETE"])
 def posts_delete(request, posts_id):
     return JsonResponse({
         "method": "DELETE",
