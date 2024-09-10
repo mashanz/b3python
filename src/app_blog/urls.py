1055a0176ba6b02f192585e5b16eb00e9e7312c0
Mashanz <mangatkk@gmail.com>
2024-09-10 20:36:01 +0700

    render: templates

--- src/app_blog/urls.py
+++ src/app_blog/urls.py
@@ -3,9 +3,16 @@ from . import views
 
 # make sure di lock setiap url atleast maksimal 1-2 method saja.
 urlpatterns = [
-    path("", views.posts_read_all),                        # GET all data           -> /
-    path("id/<str:posts_id>", views.posts_read_one),       # GET one data           -> /id/(123)
-    path("create", views.posts_create),                    # POST one data          -> /(create)
-    path("id/<str:posts_id>/update", views.posts_update),  # PATCH one data         -> /id/(123)/update
-    path("id/<str:posts_id>/delete", views.posts_delete)   # DELETE one data        -> /id/(123)/delete
+    path("api", views.apis.posts_read_all),                         # GET all data           -> /
+    path("api/id/<str:posts_id>", views.apis.posts_read_one),       # GET one data           -> /id/(123)
+    path("api/create", views.apis.posts_create),                    # POST one data          -> /(create)
+    path("api/id/<str:posts_id>/update", views.apis.posts_update),  # PATCH one data         -> /id/(123)/update
+    path("api/id/<str:posts_id>/delete", views.apis.posts_delete),  # DELETE one data        -> /id/(123)/delete
+
+    # Rendering HTML
+    path("view/read_all", views.view_read_all.view),
+    path("view/read_one", views.view_read_one.view),
+    path("view/create", views.view_create.view),
+    path("view/update", views.view_update.view),
+    path("view/delete", views.view_delete.view)
 ]
---
+++ src/app_blog/views/__init__.py
@@ -0,0 +1,6 @@
+from . import apis
+from . import view_create
+from . import view_update
+from . import view_delete
+from . import view_read_one
+from . import view_read_all
